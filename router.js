7fce16cbacd10b49e0f28f3efad55ccb2c125baf
Jack Foley <j.foley11@nuigalway.ie>
2017-03-31 17:02:43 +0100

    A guide to the nightlife of Galway City

---
+++ router.js
@@ -0,0 +1,18 @@
+Router.route('/', {
+	template: 'home'
+});
+Router.route('/aboutUs');
+Router.route('/contactUs');
+Router.route('/44club');
+Router.route('/carbon');
+Router.route('/electric');
+Router.route('/studentbar');
+Router.route('/frontdoor');
+Router.route('/pucanbar');
+Router.route('/clubList');
+Router.route('/pubList');
+Router.route('/cotw');
+Router.route('/potw');
+Router.route('/explore1');
+Router.route('/explore2');
+Router.route('/explore3');
